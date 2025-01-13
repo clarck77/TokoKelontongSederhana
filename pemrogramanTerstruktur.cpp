c55edf461908339a8d8d38061ef17ae69c7a67a7
Amrullah Azzaky <resuslampung@gmail.com>
2025-01-13 16:40:19 +0700

    feat: string barang yang akan muncul pertama kali pada menu lihat barang

--- pemrogramanTerstruktur.cpp
+++ pemrogramanTerstruktur.cpp
@@ -15,4 +15,13 @@ struct Barang {
 };
 // deklarasi struct barang - selesai
 
+// string barang pertama kali yang akan muncul
+vector<Barang> barang = {
+    {"22001", "Beras_Bulog", "KG", 25000},
+    {"22002", "Gula_Pasir", "KG", 5000},
+    {"22003", "Minyak", "Liter", 15000},
+    {"22004", "Susu_Dancow", "Gram", 20000},
+    {"22005", "Telur_Ayam", "KG", 20000}
+};
+// string barang pertama kali yang akan muncul - selesai
 
